d1d8fd067127672f2a0f0054e0838c24b03d4439
Michael <maikel_20@hotmail.es>
2018-02-16 09:22:54 +0100

    Ejercicio 0096

--- Responder.java
+++ Responder.java
@@ -14,24 +14,33 @@ public class Responder
 {
     Random azar;
     ArrayList<String> respuestas;
-    HashMap<String, String> respuestasInput;
+    HashMap<HashSet<String>, String> respuestasInput;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        
         azar = new Random();
         respuestas = new ArrayList<>();
         respuestasInput = new HashMap<>();
+        HashSet<String> set01 = new HashSet<>();
+        set01.add("atencion");
+        HashSet<String> set02 = new HashSet<>();
+        set02.add("preguntas");
+        HashSet<String> set03 = new HashSet<>();
+        set03.add("pagos");
+        HashSet<String> set04 = new HashSet<>();
+        set04.add("devolucion");
         respuestas.add("Disculpe la espera");
         respuestas.add("Le pasaremos con un operador");
         respuestas.add("¿Que es lo que necesita?");
         respuestas.add("No podemos atenderle por el momento, estamos teniendo problemas tecnicos");
         respuestas.add("¿Necesita alguna otra cosa?");
-        respuestasInput.put("atencion", "Intentamos ser lo mas rapidos posibles en atenderles, disculpen las molestias");
-        respuestasInput.put("preguntas", "Si tiene alguna duda de algo pongase en contacto con nosotros");
-        respuestasInput.put("pagos", "puede ver nuestras tarifas de pagos en nuestra pagina web");
-        respuestasInput.put("devolucion", "tratamos de ser lo mas rapido posible en devolverle su dinero, gracias por la paciencia");
+        respuestasInput.put(set01, "Intentamos ser lo mas rapidos posibles en atenderles, disculpen las molestias");
+        respuestasInput.put(set02, "Si tiene alguna duda de algo pongase en contacto con nosotros");
+        respuestasInput.put(set03, "puede ver nuestras tarifas de pagos en nuestra pagina web");
+        respuestasInput.put(set04, "tratamos de ser lo mas rapido posible en devolverle su dinero, gracias por la paciencia");
     }
 
     /**
@@ -41,19 +50,7 @@ public class Responder
     public String generateResponse(HashSet<String> input1)
     {
         String respuesta = null;
-        Iterator<String> iterator = input1.iterator();
-        boolean searching = true;
-        while(iterator.hasNext() && searching) {
-             respuesta = respuestasInput.get(iterator.next());
-             if(respuesta != null) {
-                 searching = false;
-             }
-        }
-        for(String element : input1) {
-            if(respuesta == null) {
-                respuesta = respuestasInput.get(element);
-            }
-        }
+        respuesta = respuestasInput.get(input1);
         if(respuesta == null) {
             respuesta = respuestas.get(azar.nextInt(respuestas.size()));
         }

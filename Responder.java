9261c2cc4c3101b87cded60714065e868eb90bb1
Michael <maikel_20@hotmail.es>
2018-02-16 09:02:56 +0100

    Ejercicio 0092

--- Responder.java
+++ Responder.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -12,6 +12,7 @@ public class Responder
 {
     Random azar;
     ArrayList<String> respuestas;
+    HashMap<String, String> respuestasInput;
     /**
      * Construct a Responder - nothing to do
      */
@@ -19,20 +20,30 @@ public class Responder
     {
         azar = new Random();
         respuestas = new ArrayList<>();
+        respuestasInput = new HashMap<>();
         respuestas.add("Disculpe la espera");
         respuestas.add("Le pasaremos con un operador");
         respuestas.add("¿Que es lo que necesita?");
         respuestas.add("No podemos atenderle por el momento, estamos teniendo problemas tecnicos");
         respuestas.add("¿Necesita alguna otra cosa?");
+        respuestasInput.put("atencion", "Intentamos ser lo mas rapidos posibles en atenderles, disculpen las molestias");
+        respuestasInput.put("preguntas", "Si tiene alguna duda de algo pongase en contacto con nosotros");
+        respuestasInput.put("pagos", "puede ver nuestras tarifas de pagos en nuestra pagina web");
+        respuestasInput.put("devolucion", "tratamos de ser lo mas rapido posible en devolverle su dinero, gracias por la paciencia");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String input1)
     {
+        String respuesta = null;
+        respuesta = respuestasInput.get(input1);
         int numeroDeRespuestas = azar.nextInt(respuestas.size());
+        if(respuesta == null) {
+            respuesta = respuestas.get(azar.nextInt(respuestas.size()));
+        }
         return respuestas.get(numeroDeRespuestas);
     }
 }
